690aa9936bae531d75176e5c9b2362cabf67fb78
Ian Skelskey <ianskelskey@gmail.com>
2023-11-08 01:40:10 -0500

    Initial toy drive website

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord, faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+import TextRibbon from "./assets/TextRibbon";
+function App() {
+  return (
+    <div className="min-h-screen bg-snowflake-pattern bg-repeat flex flex-col items-center p-4">
+      <img className="w-1/3" src='/ccmc-logo.png' alt="Connecticut Children's Medical Center Logo" />
+      <TextRibbon className="w-3/4" />
+      <section className="w-1/2 font-mostra-nuova bg-white p-4 bg-opacity-60 rounded-lg mb-4">
+        <h2 className="text-4xl mb-4">Welcome to the Season of Giving!</h2>
+        
+        <p className="text-xl mb-2">
+          <img className="w-24 rounded-md text-center" src="/logo512.png" alt="Toy Drive Logo"/>
+          Join the Bristol Plaza Starbucks Holiday Toy Drive
+        </p>
+        <p className="mb-2">
+          Greetings, kind hearts and joyful givers! We're delighted to welcome you to our annual Holiday Toy Drive, a special initiative dedicated to bringing smiles and joy to the children at Connecticut Children's Medical Center. As the season of cheer unfolds, your generosity can make all the difference in the lives of those who need it most.
+        </p>
+        <p className="mb-2">
+          In partnership with our compassionate community, we aim to collect a sleigh-full of toys, games, and educational items to ensure that every child experiences the magic of the holiday season. From the tiniest tots to the bravest teens, your contributions will light up faces with happiness and hope.
+        </p>
+        
+      </section>
+
+      <section className="w-1/2 font-mostra-nuova bg-white p-4 bg-opacity-60 rounded-lg mb-4">
+        <a className="text-4xl hover:text-brand-red" href='https://give.connecticutchildrens.org/team/539713' target="_blank">
+          Official Give Page
+        </a>
+      </section>
+
+      <section className="w-1/2 font-mostra-nuova bg-white p-4 bg-opacity-60 rounded-lg mb-4">
+        <h2 className="text-4xl mb-4">Connect with us</h2>
+        <div className="flex space-x-5">
+          <a href='https://discord.gg/bv2gJbGngh' target='_blank'>
+            <FontAwesomeIcon className="text-brand-teal" icon={faDiscord} size="3x" />
+          </a>
+          <a href='https://www.facebook.com/groups/sbuxtoydrive/?mibextid=oMANbw' target="_blank">
+            <FontAwesomeIcon className="text-brand-red" icon={faFacebook} size="3x" />
+          </a>
+        </div>
+      </section>
+
+      <section className="w-1/2 font-mostra-nuova bg-white p-4 bg-opacity-60 rounded-lg">
+        <h2 className="text-4xl mb-4">Community Partners</h2>
+        <div className="flex justify-around h-24">
+          <img src='/starbucks-logo.png' alt="Starbucks Logo" />
+          <img src='/tunxis-logo.png' alt="Tunxis Logo" />
+        </div>
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/Ribbon.js
@@ -0,0 +1,16 @@
+export default function Ribbon({ className }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1344 391.5"
+    >
+      <path
+        d="M159.18 0h1025.64v335.57H159.18z"
+        class="cls-1" />
+      <path
+        d="M0 55.93h149.72v289.1l46.46 44.75H3.44l98.09-165.21L0 55.93zM1344 57.65h-149.72v289.11l-46.46 44.74h192.74l-98.09-165.21L1344 57.65z"
+        class="cls-1" />
+    </svg>
+  )
+}

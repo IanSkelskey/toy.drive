bb96d5afbdb224fe841d1e05914e8b90b4c3830e
Ian Skelskey <ianskelskey@gmail.com>
2023-11-12 04:05:13 -0500

    Change font family for nav bar

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
 
         <nav className="sticky border-2 border-red-900 border-opacity-20 top-0 z-30 w-full h-14 mb-2 rounded-full bg-candy-cane-pattern">
           <div class="bg-brand-red border-2 border-red-900 border-opacity-20 absolute top-2 bottom-2 left-2 right-2 rounded-full"></div>
-          <ul className="flex justify-around h-full items-center font-etna text-white text-lg">
+          <ul className="flex justify-around h-full items-center font-bold text-white text-lg">
             <li className="h-full flex items-center px-4 z-50 hover:text-brand-yellow"><Link to="/toy.drive/">Home</Link></li>
             <li className="h-full flex items-center px-4 z-50 hover:text-brand-yellow"><Link to="/toy.drive/wishlist">Wishlist</Link></li>
             <li className="h-full flex items-center px-4 z-50 hover:text-brand-yellow"><Link to="/toy.drive/about">About Us</Link></li>

44097fa86442338944fd6c91d509ccd62320cda6
Ian Skelskey <ianskelskey@gmail.com>
2023-11-20 01:08:43 -0500

    Implement custom 404 redirect strategy

---
+++ public/redirect.js
@@ -0,0 +1,9 @@
+// redirect.js
+(function() {
+    var redirect = sessionStorage.redirect;
+    delete sessionStorage.redirect;
+    if (redirect && redirect !== window.location.href) {
+      window.history.replaceState(null, null, redirect);
+    }
+  })();
+  

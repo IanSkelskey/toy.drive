0a2da9ed4304a55731d56f584252bd2123ba8ba3
Ian Skelskey <ianskelskey@gmail.com>
2023-11-19 23:33:10 -0500

    Add keys to list children

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import navItems from '../const/navItems';
 export default function NavBar() {
     return (
         <nav className="sticky border-2 border-red-900 border-opacity-20 top-0 z-30 w-full h-14 mb-2 rounded-full bg-candy-cane-pattern">
-            <div class="bg-brand-red border-2 border-red-900 border-opacity-20 absolute top-2 bottom-2 left-2 right-2 rounded-full"></div>
+            <div className="bg-brand-red border-2 border-red-900 border-opacity-20 absolute top-2 bottom-2 left-2 right-2 rounded-full"></div>
             <ul className="flex justify-around h-full items-center font-bold text-white underline sm:text-lg text-sm">
                 {navItems.map((item) => (
-                    <li className="h-full flex items-center px-4 z-50 hover:text-brand-yellow hover:underline">
+                    <li key={item.link} className="h-full flex items-center px-4 z-50 hover:text-brand-yellow hover:underline">
                         <Link to={item.link}>{item.name}</Link>
                     </li>
                 ))}

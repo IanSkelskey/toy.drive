c67de861da1a8b575748fe4c0f5eab46f011dbca
Ian Skelskey <ianskelskey@gmail.com>
2023-11-20 21:25:41 -0500

    Correct and finalize fix public url 404 script

--- public/scripts/fix404PublicUrl.js
+++ public/scripts/fix404PublicUrl.js
@@ -1,16 +1,16 @@
-import { readFile, writeFile } from 'fs';
-import { join } from 'path';
+const { readFile, writeFile } = require('fs');
+const { join } = require('path');
 
 const publicUrl = '/toy.drive'; // Set this to your base URL path
-const filePath = join('public/scripts/', 'build', '404.html');
+const filePath = join('build', '404.html');
 
-readFile(filePath, 'utf8', function(err, data) {
+readFile(filePath, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
   var result = data.replace(/%PUBLIC_URL%/g, publicUrl);
 
-  writeFile(filePath, result, 'utf8', function(err) {
-     if (err) return console.log(err);
+  writeFile(filePath, result, 'utf8', function (err) {
+    if (err) return console.log(err);
   });
 });

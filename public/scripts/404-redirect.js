75e73e100d7a4a13d3b68d67f1cf17e8acc9b93e
Ian Skelskey <ianskelskey@gmail.com>
2023-11-20 22:05:35 -0500

    Add console logs to redirect scripts

--- public/scripts/404-redirect.js
+++ public/scripts/404-redirect.js
@@ -1,9 +1,11 @@
 // 404-redirect.js
 document.addEventListener('DOMContentLoaded', function() {
-    var redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-    if (redirect && redirect !== window.location.href) {
-      window.history.replaceState(null, null, redirect);
-    }
-  });
-  
+  var path = window.location.pathname;
+  console.log('Original path:', path);
+
+  sessionStorage.redirect = path;
+  console.log('Stored in sessionStorage:', sessionStorage.redirect);
+
+  window.location.href = '/';
+  console.log('Redirecting to home page');
+});
